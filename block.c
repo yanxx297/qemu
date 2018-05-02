1dfd1ee6373abfc62f0f7f5ae4120be222ad36de
yanxx297 <yanxx297@umn.edu>
2018-05-02 15:55:10 -0500

    Merge commit '3bd293c3fdc8b4052b9fc357e0b28cba20e73099' into pokeemu

--- block.c
+++ block.c
@@ -4378,6 +4378,9 @@ void bdrv_iostatus_reset(BlockDriverState *bs)
 {
     if (bdrv_iostatus_is_enabled(bs)) {
         bs->iostatus = BLOCK_DEVICE_IO_STATUS_OK;
+        if (bs->job) {
+            block_job_iostatus_reset(bs->job);
+        }
     }
 }
 
