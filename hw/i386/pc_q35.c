3d1d1448fbc152e61ecdd3c42d02762b1af59204
yanxx297 <yanxx297@umn.edu>
2018-05-02 16:49:54 -0500

    Merge commit b86d11ea76223b11fcd0f6c23c9f9bc7caac9367 into pokeemu

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -285,6 +285,9 @@ static void pc_q35_init(MachineState *machine)
 static void pc_compat_2_1(MachineState *machine)
 {
     smbios_uuid_encoded = false;
+    x86_cpu_compat_set_features("coreduo", FEAT_1_ECX, CPUID_EXT_VMX, 0);
+    x86_cpu_compat_set_features("core2duo", FEAT_1_ECX, CPUID_EXT_VMX, 0);
+    x86_cpu_compat_kvm_no_autodisable(FEAT_8000_0001_ECX, CPUID_EXT3_SVM);
 }
 
 static void pc_compat_2_0(MachineState *machine)
