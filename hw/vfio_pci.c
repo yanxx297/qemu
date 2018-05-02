80657238742dc79dde4769fe02577500809aade7
yanxx297 <yanxx297@umn.edu>
2018-05-02 15:54:40 -0500

    Merge commit 'd9f0e63898d639629bb7eb914786529185334635' into pokeemu

--- hw/vfio_pci.c
+++ hw/vfio_pci.c
@@ -1917,6 +1917,10 @@ static Property vfio_pci_dev_properties[] = {
     DEFINE_PROP_END_OF_LIST(),
 };
 
+static const VMStateDescription vfio_pci_vmstate = {
+    .name = "vfio-pci",
+    .unmigratable = 1,
+};
 
 static void vfio_pci_dev_class_init(ObjectClass *klass, void *data)
 {
@@ -1925,6 +1929,8 @@ static void vfio_pci_dev_class_init(ObjectClass *klass, void *data)
 
     dc->reset = vfio_pci_reset;
     dc->props = vfio_pci_dev_properties;
+    dc->vmsd = &vfio_pci_vmstate;
+    dc->desc = "VFIO-based PCI device assignment";
     pdc->init = vfio_initfn;
     pdc->exit = vfio_exitfn;
     pdc->config_read = vfio_pci_read_config;
