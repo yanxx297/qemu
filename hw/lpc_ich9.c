2baa1ef40c17c1675f410ee1f866a2b580b53631
yanxx297 <yanxx297@umn.edu>
2018-05-02 15:57:56 -0500

    Merge commit 1c4d5afeb1f962ebbdaa4c1e975a5798fd89a32f into pokeemu

--- hw/lpc_ich9.c
+++ hw/lpc_ich9.c
@@ -498,6 +498,10 @@ static int ich9_lpc_initfn(PCIDevice *d)
 
     ich9_cc_init(lpc);
     apm_init(d, &lpc->apm, ich9_apm_ctrl_changed, lpc);
+
+    lpc->machine_ready.notify = ich9_lpc_machine_ready;
+    qemu_add_machine_init_done_notifier(&lpc->machine_ready);
+
     return 0;
 }
 
