fd816ad326c4ccd5e511b657f99d39bb2e6442d7
yanxx297 <yanxx297@umn.edu>
2018-05-02 18:44:25 -0500

    Merge commit '37b5e74e28f4b3ee93c28eb0106a65e02521f48b'

--- scripts/git-submodule.sh
+++ scripts/git-submodule.sh
@@ -7,7 +7,7 @@ substat=".git-submodule-status"
 
 command=$1
 shift
-modules="$@"
+maybe_modules="$@"
 
 test -z "$GIT" && GIT=git
 
@@ -33,13 +33,19 @@ error() {
     exit 1
 }
 
-if test -z "$modules"
-then
-    test -e $substat || touch $substat
-    exit 0
-fi
+modules=""
+for m in $maybe_modules
+do
+    $GIT submodule status $m 1> /dev/null 2>&1
+    if test $? = 0
+    then
+        modules="$modules $m"
+    else
+        echo "warn: ignoring non-existent submodule $m"
+    fi
+done
 
-if ! test -e ".git"
+if test -n "$maybe_modules" && ! test -e ".git"
 then
     echo "$0: unexpectedly called with submodules but no git checkout exists"
     exit 1
@@ -47,6 +53,11 @@ fi
 
 case "$command" in
 status)
+    if test -z "$maybe_modules"
+    then
+         test -s ${substat} && exit 1 || exit 0
+    fi
+
     test -f "$substat" || exit 1
     CURSTATUS=`$GIT submodule status $modules`
     OLDSTATUS=`cat $substat`
@@ -54,13 +65,16 @@ status)
     exit $?
     ;;
 update)
-    if ! [ -e $modules ]; then
+<<<<<<< HEAD
+    if ! [ -e $modules ]; then	    
+    	if test -z "$maybe_modules"
+	    then
+        	test -e $substat || touch $substat
+	        exit 0
+	    fi
+
 	    $GIT submodule update --init $modules 1>/dev/null
 	    test $? -ne 0 && error "failed to update modules"
-	    
-	    $GIT submodule status $modules > "${substat}"
-	    test $? -ne 0 && error "failed to save git submodule status" >&2
-
     fi
     ;;
 esac
