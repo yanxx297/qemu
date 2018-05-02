8bfd5a57df0f61261f3c253954f3d7856f2e3c7f
yanxx297 <yanxx297@umn.edu>
2018-05-02 16:39:01 -0500

    Merge commit '2d9c2725f7bd08ffaf824b8c823f1423258ec78d' into pokeemu

--- vl.c
+++ vl.c
@@ -3967,6 +3967,8 @@ int main(int argc, char **argv, char **envp)
     }
     loc_set_none();
 
+    os_daemonize();
+
     if (qemu_init_main_loop()) {
         fprintf(stderr, "qemu_init_main_loop failed\n");
         exit(1);
@@ -4209,8 +4211,6 @@ int main(int argc, char **argv, char **envp)
         exit(1);
     }
 #endif
-
-    os_daemonize();
 
     if (pid_file && qemu_create_pidfile(pid_file) != 0) {
         os_pidfile_error();
