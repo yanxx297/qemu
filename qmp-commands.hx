9c07679b75da451cc4efa52a2cc7c869a698b0d8
yanxx297 <yanxx297@umn.edu>
2018-05-02 15:50:58 -0500

    Merge commit 'e4e31c6324af480a22bf4150266b278368eaa336' into pokeemu

--- qmp-commands.hx
+++ qmp-commands.hx
@@ -2381,3 +2381,9 @@ EQMP
         .mhandler.cmd_new = qmp_marshal_input_query_machines,
     },
 
+    {
+        .name       = "query-cpu-definitions",
+        .args_type  = "",
+        .mhandler.cmd_new = qmp_marshal_input_query_cpu_definitions,
+    },
+
