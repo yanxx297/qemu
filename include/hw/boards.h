3a65132f97d05b3fa9f605af5e9646efacb8617c
yanxx297 <yanxx297@umn.edu>
2018-05-02 16:30:15 -0500

    Merge commit '602a3921ffd62a08c29750d123e0d5d96acc84ee' into pokeemu

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -4,9 +4,10 @@
 #define HW_BOARDS_H
 
 #include "sysemu/blockdev.h"
-#include "sysemu/qemumachine.h"
 #include "hw/qdev.h"
 #include "qom/object.h"
+
+typedef struct QEMUMachine QEMUMachine;
 
 typedef struct QEMUMachineInitArgs {
     const QEMUMachine *machine;
